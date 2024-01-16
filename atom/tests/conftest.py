e77bde7c56dcce6ca37e5eb34a8ba5142c45903d
MagdaSlifierz <m.slifierz@gmail.com>
2024-01-15 18:04:41 -0800

    testing

--- atom/tests/conftest.py
+++ atom/tests/conftest.py
@@ -1,11 +1,11 @@
-# import pytest
-# from fastapi.testclient import TestClient
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from atom.models.database import Base, get_db
-# from atom.main import app
-# from atom.main import start_application
-#
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from atom.models.database import Base, get_db
+from atom.main import app
+from atom.main import start_application
+
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -13,29 +13,29 @@
 # TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 #
 # Base.metadata.create_all(bind=engine)
-#
-#
-# # Your override_get_db function here
+
+
+# Your override_get_db function here
 # def override_get_db():
 #     try:
 #         db = TestingSessionLocal()
 #         yield db
 #     finally:
 #         db.close()
-#
-#
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    # Create a test version of the app
+    app = start_application()
+    # app.dependency_overrides[get_db] = override_get_db
+
+    # Pass the test app to TestClient
+    client = TestClient(app)
+    yield client
+
 # @pytest.fixture(scope="module")
 # def test_app():
-#     # Create a test version of the app
-#     app = start_application()
-#     app.dependency_overrides[get_db] = override_get_db
-#
-#     # Pass the test app to TestClient
 #     client = TestClient(app)
-#     yield client
 #
-# # @pytest.fixture(scope="module")
-# # def test_app():
-# #     client = TestClient(app)
-# #
-# #     yield client
+#     yield client
